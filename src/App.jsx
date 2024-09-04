d74c9f831249c7d56412af8d333802039823a1a0
XYZ_of_properties <tushar@.com>
2024-09-04 18:42:31 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignIn from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/sign-in" element={<SignIn/>} />
@@ -15,6 +17,7 @@ export default function App() {
       <Route path="/about" element={<About/>} />
       <Route path="/profile" element={<Profile/>} />
     </Routes>
+    
     </BrowserRouter>
   )
 }
